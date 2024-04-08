ff43f78fe8e8334e25a1de08dc859bcc425edf17
Cristian Alexandru <cristi23pas@yahoo.com>
2024-04-09 00:05:58 +0300

    chore: playbar fully functional, stream service working

--- playback_service/init-db.js
+++ playback_service/init-db.js
@@ -5,7 +5,7 @@ db.songs.insertMany([
     {
         "id": 1,
         "name": "Allie_X_Downtown",
-        "path": "/var/lib/mongodb/data/Allie_X_Downtown.mp3",
+        "path": "/var/lib/songs/data/Allie_X_Downtown.mp3",
     },
     {
         "id": 2,
--- playback_service/run.py
+++ playback_service/run.py
@@ -1,8 +1,15 @@
 from flask import Flask, jsonify, send_file
-import pymongo
 from pymongo import MongoClient
+from flask_cors import CORS, cross_origin
+import pymongo
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+cors = CORS(app, resources={r"/foo": {"origins": "http://localhost:port"}})
+
 
 def get_db():
     client = MongoClient(host='mongo_db',
@@ -14,15 +21,17 @@ def get_db():
     db = client["songs_db"]
     return db
 
-@app.route('/song')
+@app.route('/song', methods=['GET'])
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def post_song():
     db = get_db()
-    song = db.songs.find_one({"id": "1"})
+    song = db.songs.find_one({"id": 1})
+
     if(song):
         file_path = song["path"]
         return send_file(file_path, mimetype='audio/mpeg')
     else:
-        return jsonify({'message': 'Song is not valid'}) 
+        return jsonify({'error': 'Song not found'}), 404
 
 @app.route('/songs')
 def fetch_songs():
